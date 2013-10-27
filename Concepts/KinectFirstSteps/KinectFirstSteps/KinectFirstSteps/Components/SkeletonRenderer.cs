d6e1d241aba412cae0942110bfe3be88242ac7f8
Vladim�r Mach <vladimir@mach.im>
2013-10-27 19:10:52 +0000

    Concepts: Move skeleton rendering to standalone component.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C37024

--- Concepts/KinectFirstSteps/KinectFirstSteps/KinectFirstSteps/Components/SkeletonRenderer.cs
+++ Concepts/KinectFirstSteps/KinectFirstSteps/KinectFirstSteps/Components/SkeletonRenderer.cs
@@ -1,113 +1,249 @@
-﻿//using Microsoft.Kinect;
-//using Microsoft.Xna.Framework;
-//using Microsoft.Xna.Framework.Graphics;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
+﻿using Microsoft.Kinect;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-//namespace KinectFirstSteps.Components
-//{
-//    public class SkeletonRenderer : DrawableGameComponent 
-//    {
-//        public SkeletonRenderer(Game game, Microsoft.Kinect.KinectSensor sensor)
-//            : base(game)
-//        {
-//        }
+namespace KinectFirstSteps.Components
+{
+    public class SkeletonRenderer : DrawableGameComponent
+    {
+        private KinectSensor kinect;
 
-//        /// <summary>
-//        /// The SpriteBatch used for rendering.
-//        /// </summary>
-//        private SpriteBatch spriteBatch;
+        public SkeletonRenderer(Game game, Microsoft.Kinect.KinectSensor sensor, SkeletonPointMap map)
+            : base(game)
+        {
+            kinect = sensor;
 
-//        /// <summary>
-//        /// Gets the selected KinectSensor.
-//        /// </summary>
-//        public KinectSensor Sensor { get; private set; }
+            mapMethod = map;
+        }
 
-//        /// <summary>
-//        /// This method initializes necessary objects.
-//        /// </summary>
-//        public override void Initialize()
-//        {
-//            base.Initialize();
+        /// <summary>
+        /// The SpriteBatch used for rendering.
+        /// </summary>
+        private SpriteBatch spriteBatch;
 
-//            this.spriteBatch = new SpriteBatch(Game.GraphicsDevice);
-//        }
+        /// <summary>
+        /// Gets the selected KinectSensor.
+        /// </summary>
+        public KinectSensor Sensor { get; private set; }
 
-//        /// <summary>
-//        /// This method renders the current state of the KinectChooser.
-//        /// </summary>
-//        /// <param name="gameTime">The elapsed game time.</param>
-//        public override void Draw(GameTime gameTime)
-//        {
-//            // If the spritebatch is null, call initialize
-//            if (this.spriteBatch == null)
-//            {
-//                this.Initialize();
-//            }
+        /// <summary>
+        /// This method initializes necessary objects.
+        /// </summary>
+        public override void Initialize()
+        {
+            base.Initialize();
 
-//            // If we don't have a sensor, or the sensor we have is not connected
-//            // then we will display the information text
-//            if (this.Sensor == null || this.LastStatus != KinectStatus.Connected)
-//            {
-//                this.spriteBatch.Begin();
+            this.spriteBatch = new SpriteBatch(Game.GraphicsDevice);
+        }
 
-//                // Render the background
-//                this.spriteBatch.Draw(
-//                    this.chooserBackground,
-//                    new Vector2(Game.GraphicsDevice.Viewport.Width / 2, Game.GraphicsDevice.Viewport.Height / 2),
-//                    null,
-//                    Color.White,
-//                    0,
-//                    new Vector2(this.chooserBackground.Width / 2, this.chooserBackground.Height / 2),
-//                    1,
-//                    SpriteEffects.None,
-//                    0);
+        private void DrawSkeleton(Vector2 resolution)
+        {
+            if (skeleton != null)
+            {
+                if (skeleton.TrackingState != SkeletonTrackingState.Tracked)
+                    return;
 
-//                // Determine the text
-//                string txt = "Required";
-//                if (this.Sensor != null)
-//                {
-//                    txt = this.statusMap[this.LastStatus];
-//                }
+                // Draw Bones
+                DrawBone(skeleton.Joints, JointType.Head, JointType.ShoulderCenter);
+                DrawBone(skeleton.Joints, JointType.ShoulderCenter, JointType.ShoulderLeft);
+                DrawBone(skeleton.Joints, JointType.ShoulderCenter, JointType.ShoulderRight);
+                DrawBone(skeleton.Joints, JointType.ShoulderCenter, JointType.Spine);
+                DrawBone(skeleton.Joints, JointType.Spine, JointType.HipCenter);
+                DrawBone(skeleton.Joints, JointType.HipCenter, JointType.HipLeft);
+                DrawBone(skeleton.Joints, JointType.HipCenter, JointType.HipRight);
 
-//                // Render the text
-//                Vector2 size = this.font.MeasureString(txt);
-//                this.spriteBatch.DrawString(
-//                    this.font,
-//                    txt,
-//                    new Vector2((Game.GraphicsDevice.Viewport.Width - size.X) / 2, (Game.GraphicsDevice.Viewport.Height / 2) + size.Y),
-//                    Color.White);
-//                this.spriteBatch.End();
-//            }
+                DrawBone(skeleton.Joints, JointType.ShoulderLeft, JointType.ElbowLeft);
+                DrawBone(skeleton.Joints, JointType.ElbowLeft, JointType.WristLeft);
+                DrawBone(skeleton.Joints, JointType.WristLeft, JointType.HandLeft);
 
-//            base.Draw(gameTime);
-//        }
+                DrawBone(skeleton.Joints, JointType.ShoulderRight, JointType.ElbowRight);
+                DrawBone(skeleton.Joints, JointType.ElbowRight, JointType.WristRight);
+                DrawBone(skeleton.Joints, JointType.WristRight, JointType.HandRight);
 
-//        /// <summary>
-//        /// This method loads the textures and fonts.
-//        /// </summary>
-//        protected override void LoadContent()
-//        {
-//            base.LoadContent();
+                DrawBone(skeleton.Joints, JointType.HipLeft, JointType.KneeLeft);
+                DrawBone(skeleton.Joints, JointType.KneeLeft, JointType.AnkleLeft);
+                DrawBone(skeleton.Joints, JointType.AnkleLeft, JointType.FootLeft);
 
-//            this.chooserBackground = Game.Content.Load<Texture2D>("ChooserBackground");
-//            this.font = Game.Content.Load<SpriteFont>("Segoe16");
-//        }
+                DrawBone(skeleton.Joints, JointType.HipRight, JointType.KneeRight);
+                DrawBone(skeleton.Joints, JointType.KneeRight, JointType.AnkleRight);
+                DrawBone(skeleton.Joints, JointType.AnkleRight, JointType.FootRight);
 
-//        /// <summary>
-//        /// This method ensures that the KinectSensor is stopped before exiting.
-//        /// </summary>
-//        protected override void UnloadContent()
-//        {
-//            base.UnloadContent();
+                // Now draw the joints
+                foreach (Joint j in skeleton.Joints)
+                {
+                    Color jointColor = Color.Green;
+                    if (j.TrackingState != JointTrackingState.Tracked)
+                    {
+                        jointColor = Color.Yellow;
+                    }
 
-//            // Always stop the sensor when closing down
-//            if (this.Sensor != null)
-//            {
-//                this.Sensor.Stop();
-//            }
-//        }
-//    }
-//}
+                    spriteBatch.Draw(
+                        this.jointTexture,
+                        this.mapMethod(j.Position),
+                        null,
+                        jointColor,
+                        0.0f,
+                        this.jointOrigin,
+                        1.0f,
+                        SpriteEffects.None,
+                        0.0f);
+                }
+            }
+        }
+
+        /// <summary>
+        /// A delegate method explaining how to map a SkeletonPoint from one space to another.
+        /// </summary>
+        /// <param name="point">The SkeletonPoint to map.</param>
+        /// <returns>The Vector2 representing the target location.</returns>
+        public delegate Vector2 SkeletonPointMap(SkeletonPoint point);
+
+        /// <summary>
+        /// The origin (center) location of the joint texture.
+        /// </summary>
+        private Vector2 jointOrigin;
+
+        /// <summary>
+        /// The joint texture.
+        /// </summary>
+        private Texture2D jointTexture;
+
+        /// <summary>
+        /// The origin (center) location of the bone texture.
+        /// </summary>
+        private Vector2 boneOrigin;
+
+        /// <summary>
+        /// The bone texture.
+        /// </summary>
+        private Texture2D boneTexture;
+
+        /// <summary>
+        /// This is the map method called when mapping from
+        /// skeleton space to the target space.
+        /// </summary>
+        private readonly SkeletonPointMap mapMethod;
+
+        private void DrawBone(JointCollection joints, JointType startJoint, JointType endJoint)
+        {
+            Vector2 start = this.mapMethod(joints[startJoint].Position);
+            Vector2 end = this.mapMethod(joints[endJoint].Position);
+            Vector2 diff = end - start;
+            Vector2 scale = new Vector2(1.0f, diff.Length() / this.boneTexture.Height);
+
+            float angle = (float)Math.Atan2(diff.Y, diff.X) - MathHelper.PiOver2;
+
+            Color color = Color.LightGreen;
+            if (joints[startJoint].TrackingState != JointTrackingState.Tracked ||
+                joints[endJoint].TrackingState != JointTrackingState.Tracked)
+            {
+                color = Color.Gray;
+            }
+
+            spriteBatch.Draw(this.boneTexture, start, null, color, angle, this.boneOrigin, scale, SpriteEffects.None, 1.0f);
+        }
+
+        /// <summary>
+        /// This method is used to map the SkeletonPoint to the color frame.
+        /// </summary>
+        /// <param name="point">The SkeletonPoint to map.</param>
+        /// <returns>A Vector2 of the location on the color frame.</returns>
+        private Vector2 SkeletonToColorMap(SkeletonPoint point)
+        {
+            if ((null != kinect) && (null != kinect.ColorStream))
+            {
+                // This is used to map a skeleton point to the color image location
+                var colorPt = kinect.CoordinateMapper.MapSkeletonPointToColorPoint(point, kinect.ColorStream.Format);
+                return new Vector2(colorPt.X, colorPt.Y);
+            }
+
+            return Vector2.Zero;
+        }
+
+        Skeleton skeleton;
+
+        public override void Update(GameTime gameTime)
+        {
+            if (kinect != null)
+            {
+                try
+                {
+                    using (SkeletonFrame skeletonFrame = kinect.SkeletonStream.OpenNextFrame(0))
+                    {
+                        if (skeletonFrame != null)
+                        {
+                            Skeleton[] skeletonData = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                            
+                            //Copy the skeleton data to our array
+                            skeletonFrame.CopySkeletonDataTo(skeletonData);
+
+                            if (skeletonData != null)
+                            {
+                                foreach (Skeleton skel in skeletonData)
+                                {
+                                    if (skel.TrackingState == SkeletonTrackingState.Tracked)
+                                    {
+                                        skeleton = skel;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //Report an error message
+                }
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This method renders the current state of the KinectChooser.
+        /// </summary>
+        /// <param name="gameTime">The elapsed game time.</param>
+        public override void Draw(GameTime gameTime)
+        {
+            // If the spritebatch is null, call initialize
+            if (this.spriteBatch == null)
+            {
+                this.Initialize();
+            }
+
+            spriteBatch.Begin();
+
+            DrawSkeleton(new Vector2(640, 480));
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        /// <summary>
+        /// This method loads the textures and fonts.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+
+            this.jointTexture = Game.Content.Load<Texture2D>("Joint");
+            this.jointOrigin = new Vector2(this.jointTexture.Width / 2, this.jointTexture.Height / 2);
+
+            this.boneTexture = Game.Content.Load<Texture2D>("Bone");
+            this.boneOrigin = new Vector2(0.5f, 0.0f);
+        }
+
+        /// <summary>
+        /// This method ensures that the KinectSensor is stopped before exiting.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+    }
+}
--- Concepts/KinectFirstSteps/KinectFirstSteps/KinectFirstSteps/Game1.cs
+++ Concepts/KinectFirstSteps/KinectFirstSteps/KinectFirstSteps/Game1.cs
@@ -16,7 +16,7 @@ namespace KinectFirstSteps
     /// <summary>
     /// This is the main type for your game
     /// </summary>
-    public class Game1 : Microsoft.Xna.Framework.Game
+    public class AtomixGame : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
@@ -28,13 +28,13 @@ namespace KinectFirstSteps
         Skeleton skeleton;
 
 
-        public Game1()
+        public AtomixGame()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
 
             // nastavíme mapper
-            mapMethod = SkeletonToColorMap;
+            //mapMethod = SkeletonToColorMap;
         }
 
         /// <summary>
@@ -46,6 +46,8 @@ namespace KinectFirstSteps
         protected override void Initialize()
         {
             InitializeKinectSensor();
+
+            Components.Add(new Components.SkeletonRenderer(this, kinect, SkeletonToColorMap));
 
             base.Initialize();
         }
@@ -158,122 +160,122 @@ namespace KinectFirstSteps
             }
         }
 
-        private void DrawSkeleton(SpriteBatch spriteBatch, Vector2 resolution, Texture2D img)
-        {
-            if (skeleton != null)
-            {
-                if (skeleton.TrackingState != SkeletonTrackingState.Tracked)
-                    return;
+        //private void DrawSkeleton(SpriteBatch spriteBatch, Vector2 resolution, Texture2D img)
+        //{
+        //    if (skeleton != null)
+        //    {
+        //        if (skeleton.TrackingState != SkeletonTrackingState.Tracked)
+        //            return;
 
-                // Draw Bones
-                DrawBone(skeleton.Joints, JointType.Head, JointType.ShoulderCenter);
-                DrawBone(skeleton.Joints, JointType.ShoulderCenter, JointType.ShoulderLeft);
-                DrawBone(skeleton.Joints, JointType.ShoulderCenter, JointType.ShoulderRight);
-                DrawBone(skeleton.Joints, JointType.ShoulderCenter, JointType.Spine);
-                DrawBone(skeleton.Joints, JointType.Spine, JointType.HipCenter);
-                DrawBone(skeleton.Joints, JointType.HipCenter, JointType.HipLeft);
-                DrawBone(skeleton.Joints, JointType.HipCenter, JointType.HipRight);
+        //        // Draw Bones
+        //        DrawBone(skeleton.Joints, JointType.Head, JointType.ShoulderCenter);
+        //        DrawBone(skeleton.Joints, JointType.ShoulderCenter, JointType.ShoulderLeft);
+        //        DrawBone(skeleton.Joints, JointType.ShoulderCenter, JointType.ShoulderRight);
+        //        DrawBone(skeleton.Joints, JointType.ShoulderCenter, JointType.Spine);
+        //        DrawBone(skeleton.Joints, JointType.Spine, JointType.HipCenter);
+        //        DrawBone(skeleton.Joints, JointType.HipCenter, JointType.HipLeft);
+        //        DrawBone(skeleton.Joints, JointType.HipCenter, JointType.HipRight);
 
-                DrawBone(skeleton.Joints, JointType.ShoulderLeft, JointType.ElbowLeft);
-                DrawBone(skeleton.Joints, JointType.ElbowLeft, JointType.WristLeft);
-                DrawBone(skeleton.Joints, JointType.WristLeft, JointType.HandLeft);
+        //        DrawBone(skeleton.Joints, JointType.ShoulderLeft, JointType.ElbowLeft);
+        //        DrawBone(skeleton.Joints, JointType.ElbowLeft, JointType.WristLeft);
+        //        DrawBone(skeleton.Joints, JointType.WristLeft, JointType.HandLeft);
 
-                DrawBone(skeleton.Joints, JointType.ShoulderRight, JointType.ElbowRight);
-                DrawBone(skeleton.Joints, JointType.ElbowRight, JointType.WristRight);
-                DrawBone(skeleton.Joints, JointType.WristRight, JointType.HandRight);
+        //        DrawBone(skeleton.Joints, JointType.ShoulderRight, JointType.ElbowRight);
+        //        DrawBone(skeleton.Joints, JointType.ElbowRight, JointType.WristRight);
+        //        DrawBone(skeleton.Joints, JointType.WristRight, JointType.HandRight);
 
-                DrawBone(skeleton.Joints, JointType.HipLeft, JointType.KneeLeft);
-                DrawBone(skeleton.Joints, JointType.KneeLeft, JointType.AnkleLeft);
-                DrawBone(skeleton.Joints, JointType.AnkleLeft, JointType.FootLeft);
+        //        DrawBone(skeleton.Joints, JointType.HipLeft, JointType.KneeLeft);
+        //        DrawBone(skeleton.Joints, JointType.KneeLeft, JointType.AnkleLeft);
+        //        DrawBone(skeleton.Joints, JointType.AnkleLeft, JointType.FootLeft);
 
-                DrawBone(skeleton.Joints, JointType.HipRight, JointType.KneeRight);
-                DrawBone(skeleton.Joints, JointType.KneeRight, JointType.AnkleRight);
-                DrawBone(skeleton.Joints, JointType.AnkleRight, JointType.FootRight);
+        //        DrawBone(skeleton.Joints, JointType.HipRight, JointType.KneeRight);
+        //        DrawBone(skeleton.Joints, JointType.KneeRight, JointType.AnkleRight);
+        //        DrawBone(skeleton.Joints, JointType.AnkleRight, JointType.FootRight);
 
-                // Now draw the joints
-                foreach (Joint j in skeleton.Joints)
-                {
-                    Color jointColor = Color.Green;
-                    if (j.TrackingState != JointTrackingState.Tracked)
-                    {
-                        jointColor = Color.Yellow;
-                    }
+        //        // Now draw the joints
+        //        foreach (Joint j in skeleton.Joints)
+        //        {
+        //            Color jointColor = Color.Green;
+        //            if (j.TrackingState != JointTrackingState.Tracked)
+        //            {
+        //                jointColor = Color.Yellow;
+        //            }
 
-                    spriteBatch.Draw(
-                        this.jointTexture,
-                        this.mapMethod(j.Position),
-                        null,
-                        jointColor,
-                        0.0f,
-                        this.jointOrigin,
-                        1.0f,
-                        SpriteEffects.None,
-                        0.0f);
-                }
+        //            spriteBatch.Draw(
+        //                this.jointTexture,
+        //                this.mapMethod(j.Position),
+        //                null,
+        //                jointColor,
+        //                0.0f,
+        //                this.jointOrigin,
+        //                1.0f,
+        //                SpriteEffects.None,
+        //                0.0f);
+        //        }
 
-                //foreach (Joint joint in skeleton.Joints)
-                //{
-                //    //if (joint.JointType != JointType.HandLeft) continue;
+        //        //foreach (Joint joint in skeleton.Joints)
+        //        //{
+        //        //    //if (joint.JointType != JointType.HandLeft) continue;
 
-                //    Color color = (joint.JointType == JointType.HandLeft || joint.JointType == JointType.HandRight) ? Color.Blue : Color.Red;
+        //        //    Color color = (joint.JointType == JointType.HandLeft || joint.JointType == JointType.HandRight) ? Color.Blue : Color.Red;
 
-                //    Vector2 position = new Vector2((((0.5f * joint.Position.X) + 0.5f) * (resolution.X)), (((-0.5f * joint.Position.Y) + 0.5f) * (resolution.Y)));
-                //    spriteBatch.Draw(img, new Rectangle(Convert.ToInt32(position.X), Convert.ToInt32(position.Y), 10, 10), color);
-                //}
-            }
-        }
+        //        //    Vector2 position = new Vector2((((0.5f * joint.Position.X) + 0.5f) * (resolution.X)), (((-0.5f * joint.Position.Y) + 0.5f) * (resolution.Y)));
+        //        //    spriteBatch.Draw(img, new Rectangle(Convert.ToInt32(position.X), Convert.ToInt32(position.Y), 10, 10), color);
+        //        //}
+        //    }
+        //}
 
-        /// <summary>
-        /// A delegate method explaining how to map a SkeletonPoint from one space to another.
-        /// </summary>
-        /// <param name="point">The SkeletonPoint to map.</param>
-        /// <returns>The Vector2 representing the target location.</returns>
-        public delegate Vector2 SkeletonPointMap(SkeletonPoint point);
+        ///// <summary>
+        ///// A delegate method explaining how to map a SkeletonPoint from one space to another.
+        ///// </summary>
+        ///// <param name="point">The SkeletonPoint to map.</param>
+        ///// <returns>The Vector2 representing the target location.</returns>
+        //public delegate Vector2 SkeletonPointMap(SkeletonPoint point);
 
-        /// <summary>
-        /// The origin (center) location of the joint texture.
-        /// </summary>
-        private Vector2 jointOrigin;
+        ///// <summary>
+        ///// The origin (center) location of the joint texture.
+        ///// </summary>
+        //private Vector2 jointOrigin;
 
-        /// <summary>
-        /// The joint texture.
-        /// </summary>
-        private Texture2D jointTexture;
+        ///// <summary>
+        ///// The joint texture.
+        ///// </summary>
+        //private Texture2D jointTexture;
 
-        /// <summary>
-        /// The origin (center) location of the bone texture.
-        /// </summary>
-        private Vector2 boneOrigin;
+        ///// <summary>
+        ///// The origin (center) location of the bone texture.
+        ///// </summary>
+        //private Vector2 boneOrigin;
 
-        /// <summary>
-        /// The bone texture.
-        /// </summary>
-        private Texture2D boneTexture;
+        ///// <summary>
+        ///// The bone texture.
+        ///// </summary>
+        //private Texture2D boneTexture;
 
-        /// <summary>
-        /// This is the map method called when mapping from
-        /// skeleton space to the target space.
-        /// </summary>
-        private readonly SkeletonPointMap mapMethod;
+        ///// <summary>
+        ///// This is the map method called when mapping from
+        ///// skeleton space to the target space.
+        ///// </summary>
+        //private readonly SkeletonPointMap mapMethod;
 
-        private void DrawBone(JointCollection joints, JointType startJoint, JointType endJoint)
-        {
-            Vector2 start = this.mapMethod(joints[startJoint].Position);
-            Vector2 end = this.mapMethod(joints[endJoint].Position);
-            Vector2 diff = end - start;
-            Vector2 scale = new Vector2(1.0f, diff.Length() / this.boneTexture.Height);
+        //private void DrawBone(JointCollection joints, JointType startJoint, JointType endJoint)
+        //{
+        //    Vector2 start = this.mapMethod(joints[startJoint].Position);
+        //    Vector2 end = this.mapMethod(joints[endJoint].Position);
+        //    Vector2 diff = end - start;
+        //    Vector2 scale = new Vector2(1.0f, diff.Length() / this.boneTexture.Height);
 
-            float angle = (float)Math.Atan2(diff.Y, diff.X) - MathHelper.PiOver2;
+        //    float angle = (float)Math.Atan2(diff.Y, diff.X) - MathHelper.PiOver2;
 
-            Color color = Color.LightGreen;
-            if (joints[startJoint].TrackingState != JointTrackingState.Tracked ||
-                joints[endJoint].TrackingState != JointTrackingState.Tracked)
-            {
-                color = Color.Gray;
-            }
+        //    Color color = Color.LightGreen;
+        //    if (joints[startJoint].TrackingState != JointTrackingState.Tracked ||
+        //        joints[endJoint].TrackingState != JointTrackingState.Tracked)
+        //    {
+        //        color = Color.Gray;
+        //    }
 
-            spriteBatch.Draw(this.boneTexture, start, null, color, angle, this.boneOrigin, scale, SpriteEffects.None, 1.0f);
-        }
+        //    spriteBatch.Draw(this.boneTexture, start, null, color, angle, this.boneOrigin, scale, SpriteEffects.None, 1.0f);
+        //}
 
         /// <summary>
         /// This method is used to map the SkeletonPoint to the color frame.
@@ -303,11 +305,11 @@ namespace KinectFirstSteps
 
             // TODO: use this.Content to load your game content here
 
-            this.jointTexture = Content.Load<Texture2D>("Joint");
-            this.jointOrigin = new Vector2(this.jointTexture.Width / 2, this.jointTexture.Height / 2);
+            //this.jointTexture = Content.Load<Texture2D>("Joint");
+            //this.jointOrigin = new Vector2(this.jointTexture.Width / 2, this.jointTexture.Height / 2);
 
-            this.boneTexture = Content.Load<Texture2D>("Bone");
-            this.boneOrigin = new Vector2(0.5f, 0.0f);
+            //this.boneTexture = Content.Load<Texture2D>("Bone");
+            //this.boneOrigin = new Vector2(0.5f, 0.0f);
         }
 
         /// <summary>
@@ -390,7 +392,7 @@ namespace KinectFirstSteps
             if (skeletonTexture != null)
             {
                 spriteBatch.Begin();
-                DrawSkeleton(spriteBatch, new Vector2(640, 480), skeletonTexture);
+                //DrawSkeleton(spriteBatch, new Vector2(640, 480), skeletonTexture);
                 //spriteBatch.Draw(skeletonTexture, new Rectangle(0, 0, 640, 480), Color.White);
 
 
