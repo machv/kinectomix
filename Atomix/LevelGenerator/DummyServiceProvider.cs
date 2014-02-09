dfb0a68478f8b7797b205a8dabc03e74fb4c8083
vladimirmach_cp <56988b42a720b33e9e462d018447a847177e7fc4rNqXCqRT>
2014-02-09 12:57:44 +0000

    Editor: Separate helper classes to own files.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C39460

---
+++ Atomix/LevelGenerator/DummyServiceProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinectomix.LevelGenerator
+{
+
+    public class DummyServiceProvider : IServiceProvider
+    {
+        public object GetService(Type serviceType)
+        {
+            return null;
+        }
+    }
+}
