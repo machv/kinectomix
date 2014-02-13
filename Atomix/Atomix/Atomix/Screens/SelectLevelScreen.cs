4b76e048a2eb124651aecce622c13fa5d33dbf34
Vladim�r Mach <vladimir@mach.im>
2014-02-13 20:05:26 +0000

    Game: Prepare level chooser screen.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C39518

---
+++ Atomix/Atomix/Atomix/Screens/SelectLevelScreen.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atomix.Screens
+{
+    public class SelectLevelScreen : IGameScreen
+    {
+        public void Update(GameTime gameTime)
+        {
+            
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            
+        }
+
+        public void LoadContent()
+        {
+            
+        }
+
+        public void UnloadContent()
+        {
+            
+        }
+    }
+}
