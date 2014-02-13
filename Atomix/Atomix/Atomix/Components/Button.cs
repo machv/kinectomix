55d377ee8ad6b250578d24f000d5c1dd200ce8f0
Vladim�r Mach <vladimir@mach.im>
2014-02-13 20:04:06 +0000

    Game: Create template for button.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C39517

---
+++ Atomix/Atomix/Atomix/Components/Button.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atomix.Components
+{
+    public class Button : DrawableGameComponent
+    {
+        public Button(Game game) : base(game)
+        {
+
+        }
+
+        protected override void LoadContent()
+        {
+            // Load textures
+
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            // detects actions like click
+
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            // Draw button
+
+            base.Draw(gameTime);
+        }
+
+        protected override void UnloadContent()
+        {
+            // Remove textures 
+
+            base.UnloadContent();
+        }
+    }
+}
