554faedeee7f92c5bcf6e082f59797010d541ad2
Vladim�r Mach <vladimir@mach.im>
2014-02-15 12:13:16 +0000

    Data: Add structure for level definitions.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C39537

---
+++ Atomix/AtomixData/LevelDefinition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AtomixData
+{
+    public class LevelDefinition
+    {
+        public enum Type
+        {
+            Compiled,
+            Serialized
+        }
+
+        public string Name { get; set; }
+
+        public string FileName { get; set; }
+
+        public Type LevelType { get; set; }
+
+        public bool IsFinished { get; set; }
+
+        public DateTime Finished { get; set; }
+    }
+}
