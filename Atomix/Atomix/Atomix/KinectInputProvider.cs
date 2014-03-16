f323fe3f65aa672c9579da12ae5767214c6a9946
Vladim�r Mach <vladimir@mach.im>
2014-03-16 15:54:29 +0000

    Game: Kinect Input provider.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C39979

---
+++ Atomix/Atomix/Atomix/KinectInputProvider.cs
@@ -0,0 +1,23 @@
+﻿using Atomix.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atomix
+{
+    public class KinectInputProvider : IInputProvider
+    {
+        protected KinectCursor _cursor;
+
+        public KinectInputProvider(KinectCursor cursor)
+        {
+            _cursor = cursor;
+        }
+
+        public IInputState GetState()
+        {
+            return new KinectInputState(_cursor);
+        }
+    }
+}
---
+++ Atomix/Atomix/Atomix/KinectInputState.cs
@@ -0,0 +1,33 @@
+﻿using Atomix.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atomix
+{
+    public class KinectInputState : IInputState
+    {
+        protected KinectCursor _cursor;
+
+        public int X
+        {
+            get { return (int)_cursor.HandPosition.X; }
+        }
+
+        public int Y
+        {
+            get { return (int)_cursor.HandPosition.Y; }
+        }
+
+        public bool IsSelected
+        {
+            get { return _cursor.IsHandClosed || _cursor.IsHandPressed; }
+        }
+
+        public KinectInputState(KinectCursor cursor)
+        {
+            _cursor = cursor;
+        }
+    }
+}
