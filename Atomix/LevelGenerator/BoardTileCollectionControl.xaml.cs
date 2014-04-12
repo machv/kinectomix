32ed0cdf6ce883f6a3d231759e2ed0a63d6d4fa2
Vladim�r Mach <vladimir@mach.im>
2014-04-12 09:35:05 +0000

    Generator: Mouse Enter/Leave.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C40361

--- Atomix/LevelGenerator/BoardTileCollectionControl.xaml.cs
+++ Atomix/LevelGenerator/BoardTileCollectionControl.xaml.cs
@@ -83,6 +83,8 @@ namespace Kinectomix.LevelGenerator
             AtomixData.BoardTile tile = ((ContentPresenter)ItemsControl.ContainerFromElement((ItemsControl)sender, (DependencyObject)e.OriginalSource)).Content as AtomixData.BoardTile;
 
             RaiseTileSelectedEvent(tile);
+
+            DragDrop.DoDragDrop((DependencyObject)e.OriginalSource, tile, DragDropEffects.Move);
         }
 
         private void Button_MouseEnter(object sender, MouseEventArgs e)
