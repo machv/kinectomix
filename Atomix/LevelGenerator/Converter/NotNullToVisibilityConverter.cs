ac266ad4ca5cad577f7d5655d066c1465611a4af
Vladim�r Mach <vladimir@mach.im>
2014-04-16 22:09:35 +0000

    Editor: Levels layout draft.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C40455

--- Atomix/LevelGenerator/Converter/NotNullToVisibilityConverter.cs
+++ Atomix/LevelGenerator/Converter/NotNullToVisibilityConverter.cs
@@ -4,15 +4,16 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
-namespace Kinectomix.LevelGenerator.Converter
+namespace Kinectomix.LevelGenerator
 {
-    public class NotNullToBooleanConverter : IValueConverter
+    public class NotNullToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value != null;
+            return value != null ? Visibility.Visible : Visibility.Collapsed;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
---
+++ Atomix/LevelGenerator/ViewModel/LevelsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Kinectomix.LevelGenerator.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinectomix.LevelGenerator.ViewModel
+{
+    public class LevelsViewModel : NotifyPropertyBase
+    {
+    }
+}
