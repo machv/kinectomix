054a535a99b7c44496075976069cea0e1ceb6b9d
Vladim�r Mach <vladimir@mach.im>
2014-04-26 12:09:39 +0000

    Logic: Remove usings.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C40607

--- Atomix/LevelGenerator/Mvvm/NotifyPropertyBase.cs
+++ Atomix/LevelGenerator/Mvvm/NotifyPropertyBase.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 // http://www.markwithall.com/programming/2013/03/01/worlds-simplest-csharp-wpf-mvvm-example.html
 namespace Kinectomix.LevelGenerator.Mvvm
