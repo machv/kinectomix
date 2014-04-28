8da7b6c68a361746774f09e360a65557494d888a
Vladim�r Mach <vladimir@mach.im>
2014-04-28 20:02:57 +0000

    Game: Testing not-null when loading level.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C40639

--- Atomix/Atomix/Atomix/LevelFactory.cs
+++ Atomix/Atomix/Atomix/LevelFactory.cs
@@ -8,14 +8,22 @@ namespace Atomix
         public static LevelViewModel ToViewModel(Level level)
         {
             LevelViewModel levelVm = new LevelViewModel();
-            levelVm.Board = new BoardCollection<BoardTileViewModel>(level.Board.RowsCount, level.Board.ColumnsCount);
-            levelVm.Molecule = new BoardCollection<BoardTileViewModel>(level.Molecule.RowsCount, level.Molecule.ColumnsCount);
 
-            foreach (BoardTile tile in level.Board)
-                levelVm.Board.Add(new BoardTileViewModel(tile));
+            if (level.Board != null)
+            {
+                levelVm.Board = new BoardCollection<BoardTileViewModel>(level.Board.RowsCount, level.Board.ColumnsCount);
 
-            foreach (BoardTile tile in level.Molecule)
-                levelVm.Molecule.Add(new BoardTileViewModel(tile));
+                foreach (BoardTile tile in level.Board)
+                    levelVm.Board.Add(new BoardTileViewModel(tile));
+            }
+
+            if (level.Molecule != null)
+            {
+                levelVm.Molecule = new BoardCollection<BoardTileViewModel>(level.Molecule.RowsCount, level.Molecule.ColumnsCount);
+
+                foreach (BoardTile tile in level.Molecule)
+                    levelVm.Molecule.Add(new BoardTileViewModel(tile));
+            }
 
             return levelVm;
         }
