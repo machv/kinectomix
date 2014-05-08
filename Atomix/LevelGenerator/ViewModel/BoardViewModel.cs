d94e0593e65d1a1d0a8cc36ce0afafec5f0543ca
Vladim�r Mach <vladimir@mach.im>
2014-05-08 21:28:48 +0000

    Editor: Columns editing.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C40794

--- Atomix/LevelGenerator/ViewModel/BoardViewModel.cs
+++ Atomix/LevelGenerator/ViewModel/BoardViewModel.cs
@@ -61,6 +61,10 @@ namespace Kinectomix.LevelGenerator.ViewModel
             _insertRowToBottomCommand = new DelegateCommand(InsertRowToBottom, CanInsertRowToBottom);
             _removeRowFromTopCommand = new DelegateCommand(RemoveRowFromTop, CanRemoveRowFromTop);
             _removeRowFromBottomCommand = new DelegateCommand(RemoveRowFromBottom, CanRemoveRowFromBottom);
+            _insertColumnToTopCommand = new DelegateCommand(InsertColumnToTop, CanInsertRowToTop);
+            _insertColumnToBottomCommand = new DelegateCommand(InsertColumnToBottom, CanInsertColumnToBottom);
+            _removeColumnFromTopCommand = new DelegateCommand(RemoveColumnFromTop, CanRemoveColumnFromTop);
+            _removeColumnFromBottomCommand = new DelegateCommand(RemoveColumnFromBottom, CanRemoveColumnFromBottom);
         }
         public BoardViewModel(int rowsCount, int columnsCount) : this()
         {
@@ -97,6 +101,10 @@ namespace Kinectomix.LevelGenerator.ViewModel
         private DelegateCommand _insertRowToBottomCommand;
         private DelegateCommand _removeRowFromTopCommand;
         private DelegateCommand _removeRowFromBottomCommand;
+        private DelegateCommand _insertColumnToTopCommand;
+        private DelegateCommand _insertColumnToBottomCommand;
+        private DelegateCommand _removeColumnFromTopCommand;
+        private DelegateCommand _removeColumnFromBottomCommand;
 
         public ICommand InsertRowToTopCommand
         {
@@ -113,6 +121,22 @@ namespace Kinectomix.LevelGenerator.ViewModel
         public ICommand RemoveRowFromBottomCommand
         {
             get { return _removeRowFromBottomCommand; }
+        }
+        public ICommand InsertColumnToTopCommand
+        {
+            get { return _insertColumnToTopCommand; }
+        }
+        public ICommand InsertColumnToBottomCommand
+        {
+            get { return _insertColumnToBottomCommand; }
+        }
+        public ICommand RemoveColumnFromTopCommand
+        {
+            get { return _removeColumnFromTopCommand; }
+        }
+        public ICommand RemoveColumnFromBottomCommand
+        {
+            get { return _removeColumnFromBottomCommand; }
         }
 
         protected void InsertRowToTop()
@@ -158,6 +182,49 @@ namespace Kinectomix.LevelGenerator.ViewModel
         {
             return Tiles.RowsCount > 1;
         }
+        protected void InsertColumnToTop()
+        {
+            Tiles.InsertRow(0);
+
+            RaiseChanged();
+        }
+        private bool CanInsertColumnToTop(object parameter)
+        {
+            return true;
+        }
+
+        protected void InsertColumnToBottom()
+        {
+            Tiles.InsertColumn(Tiles.ColumnsCount);
+
+            RaiseChanged();
+        }
+        private bool CanInsertColumnToBottom(object parameter)
+        {
+            return true;
+        }
+
+        protected void RemoveColumnFromTop()
+        {
+            Tiles.RemoveColumn(0);
+
+            RaiseChanged();
+        }
+        private bool CanRemoveColumnFromTop(object parameter)
+        {
+            return Tiles.ColumnsCount > 1;
+        }
+
+        protected void RemoveColumnFromBottom()
+        {
+            Tiles.RemoveColumn(Tiles.ColumnsCount - 1);
+
+            RaiseChanged();
+        }
+        private bool CanRemoveColumnFromBottom(object parameter)
+        {
+            return Tiles.ColumnsCount > 1;
+        }
 
         private void RaiseChanged()
         {
@@ -165,6 +232,10 @@ namespace Kinectomix.LevelGenerator.ViewModel
             _removeRowFromBottomCommand.RaiseCanExecuteChanged();
             _insertRowToTopCommand.RaiseCanExecuteChanged();
             _insertRowToBottomCommand.RaiseCanExecuteChanged();
+            _removeColumnFromTopCommand.RaiseCanExecuteChanged();
+            _removeColumnFromBottomCommand.RaiseCanExecuteChanged();
+            _insertColumnToTopCommand.RaiseCanExecuteChanged();
+            _insertColumnToBottomCommand.RaiseCanExecuteChanged();
         }
 
         public void PopulateEmptyTiles(BoardTileViewModel emptyTileTemplate)
