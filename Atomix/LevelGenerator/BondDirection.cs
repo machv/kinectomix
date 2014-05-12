9b23fde4833635d043f9b2b89a67304148d0a949
Vladim�r Mach <vladimir@mach.im>
2014-05-12 19:53:28 +0000

    Editor: Add constants for bond directions.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C40867

---
+++ Atomix/LevelGenerator/BondDirection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinectomix.LevelGenerator
+{
+    public class BondDirection
+    {
+        public const short Top = 0;
+        public const short TopRight = 45;
+        public const short Right = 90;
+        public const short BottomRight = 135;
+        public const short Bottom = 180;
+        public const short BottomLeft = 225;
+        public const short Left = 270;
+        public const short TopLeft = 315;
+    }
+}
