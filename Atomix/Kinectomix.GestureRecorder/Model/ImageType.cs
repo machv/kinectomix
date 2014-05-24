4a9fe886713791ffb79ddcdb3cf94f7046a3f0b6
Vladim�r Mach <vladimir@mach.im>
2014-05-24 16:26:27 +0000

    Gestures: Add skeleton rendering control.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C41093

---
+++ Atomix/Kinectomix.GestureRecorder/Model/ImageType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinectomix.GestureRecorder.Model
+{
+    public enum ImageType
+    {
+        /// <summary>
+        /// The Color Image
+        /// </summary>
+        Color,
+
+        /// <summary>
+        /// The Depth Image
+        /// </summary>
+        Depth,
+    }
+}
---
+++ Atomix/Kinectomix.GestureRecorder/Model/JointMapping.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kinectomix.GestureRecorder.Model
+{
+    /// <summary>
+    /// This class is used to map points between skeleton and color/depth
+    /// </summary>
+    public class JointMapping
+    {
+        /// <summary>
+        /// Gets or sets the joint at which we we are looking
+        /// </summary>
+        public Joint Joint { get; set; }
+
+        /// <summary>
+        /// Gets or sets the point mapped into the target display
+        /// </summary>
+        public Point MappedPoint { get; set; }
+    }
+}
