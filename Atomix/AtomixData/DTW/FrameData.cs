01626b05700fabb7d12fe255ccbe2bf77a28bb34
Vladim�r Mach <vladimir@mach.im>
2014-05-25 22:21:50 +0000

    Gestures.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C41111

---
+++ Atomix/AtomixData/DTW/FrameData.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kinectomix.Logic.DTW
+{
+    public class FrameData
+    {
+        public SkeletonPoint[] SkeletonJoints { get; set; }
+
+        public int Capacity { get; private set; }
+
+        public FrameData(int capacity)
+        {
+            Capacity = capacity;
+            SkeletonJoints = new JointType[capacity];
+        }
+    }
+}
---
+++ Atomix/AtomixData/DTW/GestureProcessor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kinectomix.Logic.DTW
+{
+    public class GestureProcessor
+    {
+        public const int TrackedJointsCount = 20;
+
+        Queue<FrameData> FrameBuffer;
+
+        private static JointType[] _tracableSkeletonJoints;
+
+        static GestureProcessor()
+        {
+            _tracableSkeletonJoints = (JointType[])Enum.GetValues(typeof(JointType));
+        }
+
+        public void AddGesture(Gesture gesture)
+        {
+
+        }
+
+        public void ProcessSkeleton(Skeleton skeleton)
+        {
+            // normalizovat body
+            //skeleton.Joints
+
+            FrameData frame = new FrameData(TrackedJointsCount);
+
+            foreach(JointType joint in _tracableSkeletonJoints)
+                frame.SkeletonJoints[(int)joint] = new SkeletonPoint() { X = 0, Y = 0, Z = 0 };
+        }
+    }
+}
