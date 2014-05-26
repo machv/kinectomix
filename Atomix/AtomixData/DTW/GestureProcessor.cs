7e05aea27f12129dd950b2c8af5d2f2704cb2a74
Vladim�r Mach <vladimir@mach.im>
2014-05-26 18:27:02 +0000

    Gestures: Saving frames.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C41129

--- Atomix/AtomixData/DTW/GestureProcessor.cs
+++ Atomix/AtomixData/DTW/GestureProcessor.cs
@@ -9,14 +9,20 @@ namespace Kinectomix.Logic.DTW
     public class GestureProcessor
     {
         public const int TrackedJointsCount = 20;
+        public const int BufferLength = 30;
 
-        Queue<FrameData> FrameBuffer;
+        Queue<FrameData> _frameBuffer;
 
         private static JointType[] _tracableSkeletonJoints;
 
         static GestureProcessor()
         {
             _tracableSkeletonJoints = (JointType[])Enum.GetValues(typeof(JointType));
+        }
+
+        public GestureProcessor()
+        {
+            _frameBuffer = new Queue<FrameData>(BufferLength);
         }
 
         public void AddGesture(Gesture gesture)
@@ -26,13 +32,41 @@ namespace Kinectomix.Logic.DTW
 
         public void ProcessSkeleton(Skeleton skeleton)
         {
-            // normalizovat body
-            //skeleton.Joints
+            // transpozice a normalizace souřadnic, jednotkou je délka krku (ShoulderCenter - Head) 
+
+            if (skeleton.Joints[JointType.Head].TrackingState != JointTrackingState.Tracked || 
+                skeleton.Joints[JointType.ShoulderCenter].TrackingState != JointTrackingState.Tracked)
+                return;
+
+            SkeletonPoint center = new SkeletonPoint()
+            {
+                X = (skeleton.Joints[JointType.ShoulderCenter].Position.X + skeleton.Joints[JointType.Head].Position.X) / 2,
+                Y = (skeleton.Joints[JointType.ShoulderCenter].Position.Y + skeleton.Joints[JointType.Head].Position.Y) / 2,
+                Z = (skeleton.Joints[JointType.ShoulderCenter].Position.Z + skeleton.Joints[JointType.Head].Position.Z) / 2,
+            };
+
+            float neckLength = (float)Math.Sqrt(
+                    Math.Pow((skeleton.Joints[JointType.ShoulderCenter].Position.X - skeleton.Joints[JointType.Head].Position.X), 2) +
+                    Math.Pow((skeleton.Joints[JointType.ShoulderCenter].Position.Y - skeleton.Joints[JointType.Head].Position.Y), 2) +
+                    Math.Pow((skeleton.Joints[JointType.ShoulderCenter].Position.Z - skeleton.Joints[JointType.Head].Position.Z), 2)
+                );
 
             FrameData frame = new FrameData(TrackedJointsCount);
 
-            foreach(JointType joint in _tracableSkeletonJoints)
-                frame.SkeletonJoints[(int)joint] = new SkeletonPoint() { X = 0, Y = 0, Z = 0 };
+            foreach (JointType joint in _tracableSkeletonJoints)
+            {
+                frame.SkeletonJoints[(int)joint] = new SkeletonPoint()
+                {
+                    X = (skeleton.Joints[joint].Position.X - center.X) / neckLength,
+                    Y = (skeleton.Joints[joint].Position.Y - center.Y) / neckLength,
+                    Z = (skeleton.Joints[joint].Position.Z - center.Z) / neckLength,
+                };
+            }
+
+            if (_frameBuffer.Count > BufferLength)
+                _frameBuffer.Dequeue();
+
+            _frameBuffer.Enqueue(frame);
         }
     }
 }
