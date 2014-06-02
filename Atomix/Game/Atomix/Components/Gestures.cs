0922fc1199a89c7d2dd505ad4f3b9cb42193eeaa
Vladim�r Mach <vladimir@mach.im>
2014-06-02 19:43:52 +0000

    Game: Task processing of skeletons.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C41284

--- Atomix/Game/Atomix/Components/Gestures.cs
+++ Atomix/Game/Atomix/Components/Gestures.cs
@@ -12,6 +12,9 @@ using Kinectomix.Logic.Gestures;
 using System.Xml.Serialization;
 using System.IO;
 using Kinectomix.Logic.Game;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using Microsoft.Kinect;
 
 namespace Atomix.Components
 {
@@ -70,16 +73,63 @@ namespace Atomix.Components
             base.Initialize();
         }
 
+        private readonly object _locker = new object();
+        private bool _isUpdating = false;
+        private ConcurrentQueue<Skeleton> _pendingSkeletons = new ConcurrentQueue<Skeleton>();
+        private ConcurrentQueue<RecognizedGesture> _recognizedGestures = new ConcurrentQueue<RecognizedGesture>();
+
         /// <summary>
         /// Allows the game component to update itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         public override void Update(GameTime gameTime)
         {
-            _recognizer.ProcessSkeleton(_skeletons.TrackedSkeleton);
-            _recognizedGesture = _recognizer.RecognizedGesture;
+            lock (_locker)
+            {
+                if (_isUpdating == false && _pendingSkeletons.Count > 0)
+                {
+                    Task.Factory.StartNew(() => ProcessRecognizing());
+                }
+                else
+                {
+                    _pendingSkeletons.Enqueue(_skeletons.TrackedSkeleton);
+                }
+            }
 
             base.Update(gameTime);
+        }
+
+        public void ProcessRecognizing()
+        {
+            if (_pendingSkeletons.Count == 0)
+                return;
+
+            lock (_locker)
+            {
+                _isUpdating = true;
+            }
+
+            Skeleton[] skeletons = new Skeleton[_pendingSkeletons.Count];
+            for (int i = 0; i < skeletons.Length; i++)
+            {
+                Skeleton skeleton;
+                if (_pendingSkeletons.TryDequeue(out skeleton))
+                    skeletons[i] = skeleton;
+            }
+
+            foreach (Skeleton skeleton in skeletons)
+            {
+                _recognizer.ProcessSkeleton(skeleton);
+                if (_recognizer.RecognizedGesture != null)
+                {
+                    _recognizedGestures.Enqueue(_recognizer.RecognizedGesture);
+                }
+            }
+
+            lock (_locker)
+            {
+                _isUpdating = false;
+            }
         }
     }
 }
