afafe785bf404eb63ae160496d865bb0a42d8219
Vladim�r Mach <vladimir@mach.im>
2014-06-08 15:37:23 +0000

    Use double in Animated texture; Remove depth field; Add option to custom texture.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C41392

--- Atomix/Game/Atomix/AnimatedTexture.cs
+++ Atomix/Game/Atomix/AnimatedTexture.cs
@@ -10,26 +10,30 @@ namespace Atomix
         private Texture2D myTexture;
         private float TimePerFrame;
         private int frame;
-        private float TotalElapsed;
+        private double TotalElapsed;
         private bool Paused;
 
         public int Frame { get { return frame; } }
 
-        public float Rotation, Scale, Depth;
+        public float Rotation, Scale;
         public Vector2 Origin;
-        public AnimatedTexture(Vector2 origin, float rotation,
-            float scale, float depth)
+        public AnimatedTexture(Vector2 origin, float rotation, float scale)
         {
             Origin = origin;
             Rotation = rotation;
             Scale = scale;
-            Depth = depth;
         }
-        public void Load(ContentManager content, string asset,
-            int frameCount, int framesPerSec)
+        public void Load(ContentManager content, string asset, int frameCount, int framesPerSec)
+        {
+            Texture2D texture = content.Load<Texture2D>(asset);
+
+            Load(texture, frameCount, framesPerSec);
+        }
+
+        public void Load(Texture2D texture, int frameCount, int framesPerSec)
         {
             framecount = frameCount;
-            myTexture = content.Load<Texture2D>(asset);
+            myTexture = texture;
             TimePerFrame = (float)1 / framesPerSec;
             frame = 0;
             TotalElapsed = 0;
@@ -37,10 +41,11 @@ namespace Atomix
         }
 
         // class AnimatedTexture
-        public void UpdateFrame(float elapsed)
+        public void UpdateFrame(double elapsed)
         {
             if (Paused)
                 return;
+
             TotalElapsed += elapsed;
             if (TotalElapsed > TimePerFrame)
             {
@@ -62,7 +67,7 @@ namespace Atomix
             Rectangle sourcerect = new Rectangle(FrameWidth * frame, 0,
                 FrameWidth, myTexture.Height);
             batch.Draw(myTexture, screenPos, sourcerect, Color.White,
-                Rotation, Origin, Scale, SpriteEffects.None, Depth);
+                Rotation, Origin, Scale, SpriteEffects.None, 0);
         }
 
         public bool IsPaused
