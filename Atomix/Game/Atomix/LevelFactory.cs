c949546e99aab04d62a99e7c6c949f0cac4a3714
Vladim�r Mach <vladimir@mach.im>
2014-06-09 19:24:35 +0000

    Game: Move creating of view model from level factory.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C41426

--- Atomix/Game/Atomix/LevelFactory.cs
+++ Atomix/Game/Atomix/LevelFactory.cs
@@ -17,47 +17,5 @@ namespace Atomix
             using (Stream stream = TitleContainer.OpenStream(path))
                 return seralizer.Deserialize(stream) as Level;
         }
-
-        public static LevelViewModel ToViewModel(Level level, GraphicsDevice graphicsDevice)
-        {
-            LevelViewModel levelVm = new LevelViewModel();
-
-            if (level.Board != null)
-            {
-                levelVm.Board = new TilesCollection<BoardTileViewModel>(level.Board.RowsCount, level.Board.ColumnsCount);
-
-                foreach (BoardTile tile in level.Board)
-                {
-                    BoardTileViewModel tileVm = tile != null ? new BoardTileViewModel(tile) : null;
-                    levelVm.Board.Add(tileVm);
-                }
-            }
-
-            if (level.Molecule != null)
-            {
-                levelVm.Molecule = new TilesCollection<BoardTileViewModel>(level.Molecule.RowsCount, level.Molecule.ColumnsCount);
-
-                foreach (BoardTile tile in level.Molecule)
-                {
-                    BoardTileViewModel tileVm = tile != null ? new BoardTileViewModel(tile) : null;
-                    levelVm.Molecule.Add(tileVm);
-                }
-            }
-
-            // Load assets
-            levelVm.Assets = new Dictionary<string, Texture2D>();
-            foreach (var asset in level.Assets)
-            {
-                using (Stream stream = new MemoryStream(asset.DecodedAssetContent))
-                {
-                    Texture2D texture = Texture2D.FromStream(graphicsDevice, stream);
-
-                    string key = asset.AssetCode != null ? asset.AssetCode : asset.AssetName;
-                    levelVm.Assets.Add(key, texture);
-                }
-            }
-
-            return levelVm;
-        }
     }
 }
--- Atomix/Game/Atomix/ViewModel/LevelViewModel.cs
+++ Atomix/Game/Atomix/ViewModel/LevelViewModel.cs
@@ -1,16 +1,72 @@
 ﻿using Kinectomix.Logic;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Atomix.ViewModel
 {
+    /// <summary>
+    /// View Model for level definition.
+    /// </summary>
     public class LevelViewModel
     {
+        private Level _level;
+
+        /// <summary>
+        /// Gets <see cref="Level"/> model for which is this View Model created.
+        /// </summary>
+        /// <returns>Model</returns>
+        public Level Level
+        {
+            get { return _level; }
+        }
+
+        /// <summary>
+        /// Gets or sets friendly name of the level.
+        /// </summary>
+        /// <returns>Friendly name of the level.</returns>
+        public string Name
+        {
+            get { return _level.Name; }
+            set { _level.Name = value; }
+        }
+
         public TilesCollection<BoardTileViewModel> Board { get; set; }
 
         public TilesCollection<BoardTileViewModel> Molecule { get; set; }
 
         public Dictionary<string, Texture2D> Assets { get; set; }
+
+        /// <summary>
+        /// Creates new instance of <see cref="LevelViewModel"/>.
+        /// </summary>
+        /// <param name="level">Model class for this View Model.</param>
+        public LevelViewModel(Level level)
+        {
+            _level = level;
+
+            if (level.Board != null)
+            {
+                Board = new TilesCollection<BoardTileViewModel>(level.Board.RowsCount, level.Board.ColumnsCount);
+
+                foreach (BoardTile tile in level.Board)
+                {
+                    BoardTileViewModel tileVm = tile != null ? new BoardTileViewModel(tile) : null;
+                    Board.Add(tileVm);
+                }
+            }
+
+            if (level.Molecule != null)
+            {
+                Molecule = new TilesCollection<BoardTileViewModel>(level.Molecule.RowsCount, level.Molecule.ColumnsCount);
+
+                foreach (BoardTile tile in level.Molecule)
+                {
+                    BoardTileViewModel tileVm = tile != null ? new BoardTileViewModel(tile) : null;
+                    Molecule.Add(tileVm);
+                }
+            }
+        }
 
         public bool CanGoUp(int x, int y)
         {
@@ -42,6 +98,26 @@ namespace Atomix.ViewModel
                 return true;
 
             return false;
+        }
+
+        public static LevelViewModel FromModel(Level level, GraphicsDevice graphicsDevice)
+        {
+            LevelViewModel levelVm = new LevelViewModel(level);
+
+            // Load assets
+            levelVm.Assets = new Dictionary<string, Texture2D>();
+            foreach (var asset in level.Assets)
+            {
+                using (Stream stream = new MemoryStream(asset.DecodedAssetContent))
+                {
+                    Texture2D texture = Texture2D.FromStream(graphicsDevice, stream);
+
+                    string key = asset.AssetCode != null ? asset.AssetCode : asset.AssetName;
+                    levelVm.Assets.Add(key, texture);
+                }
+            }
+
+            return levelVm;
         }
     }
 }
