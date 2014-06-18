0e090ae28870f06b4f9376fbc0225ff75e4dfe07
Vladim�r Mach <vladimir@mach.im>
2014-06-18 20:26:55 +0000

    Document Level class.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C41635

--- Atomix/Logic/Level.cs
+++ Atomix/Logic/Level.cs
@@ -3,20 +3,56 @@ using System.Collections.Generic;
 
 namespace Kinectomix.Logic
 {
+    /// <summary>
+    /// Definition of one game level.
+    /// </summary>
     [Serializable]
     public class Level
     {
-        public List<LevelAsset> Assets { get; set; }
+        private List<LevelAsset> _assets;
+        private string _name;
+        private TilesCollection<BoardTile> _board;
+        private TilesCollection<BoardTile> _molecule;
 
+        /// <summary>
+        /// Gets or sets list of binary assets used in the level.
+        /// </summary>
+        /// <returns></returns>
+        public List<LevelAsset> Assets
+        {
+            get { return _assets; }
+            set { _assets = value; }
+        }
         /// <summary>
         /// Gets or sets friendly name of the level.
         /// </summary>
         /// <returns>Friendly name of the level.</returns>
-        public string Name { get; set; }
-
-        public TilesCollection<BoardTile> Board { get; set; }
-        public TilesCollection<BoardTile> Molecule { get; set; }
-
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        /// <summary>
+        /// Gets or sets <see cref="TilesCollection{T}"/> containing game tiles.
+        /// </summary>
+        /// <returns>Game tiles collection.</returns>
+        public TilesCollection<BoardTile> Board
+        {
+            get { return _board; }
+            set { _board = value; }
+        }
+        /// <summary>
+        /// Gets or sets <see cref="TilesCollection{T}"/> containing tiles describing molecule definition.
+        /// </summary>
+        /// <returns>Molecule definition.</returns>
+        public TilesCollection<BoardTile> Molecule
+        {
+            get { return _molecule; }
+            set { _molecule = value; }
+        }
+        /// <summary>
+        /// Initializes new instance of the <see cref="Level"/> class.
+        /// </summary>
         public Level()
         {
             Assets = new List<LevelAsset>();
