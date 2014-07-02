0b6798e8821b636ddba47a3f7d202cb26deca74d
Vladim�r Mach <vladimir@mach.im>
2014-07-02 22:35:19 +0000

    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C41814

--- Atomix/Logic/Bresenham.cs
+++ Atomix/Logic/Bresenham.cs
@@ -49,25 +49,5 @@ namespace AtomixData
                 }
             }
         }
-     //   function line(x0, y0, x1, y1)
-     //boolean steep := abs(y1 - y0) > abs(x1 - x0)
-     //if steep then
-     //    swap(x0, y0)
-     //    swap(x1, y1)
-     //if x0 > x1 then
-     //    swap(x0, x1)
-     //    swap(y0, y1)
-     //int deltax := x1 - x0
-     //int deltay := abs(y1 - y0)
-     //int error := deltax / 2
-     //int ystep
-     //int y := y0
-     //if y0<y1 then ystep := 1 else ystep := -1
-     //for x from x0 to x1
-     //    if steep then plot(y,x) else plot(x, y)
-     //    error := error - deltay
-     //    if error< 0 then
-     //        y := y + ystep
-     //        error := error + deltax
     }
 }
