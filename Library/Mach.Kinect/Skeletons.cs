d222892eb904addd75f191f30a70792cc4d443d5
Vladim�r Mach <vladimir@mach.im>
2014-07-10 19:18:18 +0000

    Tracking types.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C41896

--- Library/Mach.Kinect/Skeletons.cs
+++ Library/Mach.Kinect/Skeletons.cs
@@ -7,37 +7,82 @@ namespace Mach.Kinect
     {
         private long _timestamp;
 
-        public enum TrackingType { Nearest, FixById }
+        public enum SkeletonType
+        {
+            /// <summary>
+            /// The nearest fully tracked skeleton from the sensor.
+            /// </summary>
+            NearestTracked,
+            /// <summary>
+            /// The nearest skeleton that has at least known position from the sensor.
+            /// </summary>
+            Nearest,
+            /// <summary>
+            /// The skeleton with corresponding ID.
+            /// </summary>
+            FixById,
+            /// <summary>
+            /// The skeleton with corresponding ID and if that ID is not tracked it tries to find first alternative with at least known position.
+            /// </summary>
+            FixByIdOrAny,
+            /// <summary>
+            /// The skeleton with corresponding ID and if that ID is not tracked it tries to find first fully tracked alternative.
+            /// </summary>
+            FixByIdOrAnyTracked,
+        }
 
         public Skeleton[] Items { get; private set; }
 
-        public TrackingType Tracking { get; set; }
+        private SkeletonType _tracking;
+        public SkeletonType Tracking
+        {
+            get { return _tracking; }
+            set { _tracking = value; }
+        }
 
-        private int trackedId = -1;
+        private int _trackedSkeletonId = -1;
+
+        public int TrackedSkeletonId
+        {
+            get { return _trackedSkeletonId; }
+            set { _trackedSkeletonId = value; }
+        }
+
         public Skeleton TrackedSkeleton
         {
             get
             {
-                if (Tracking == TrackingType.FixById)
-                {
-                    if (trackedId == -1)
-                    {
-                        FixToFirstTrackedSkeleton();
-                    }
+                Skeleton skeleton;
 
-                    Skeleton s = GetSkeleton(trackedId);
-                    if (s == null)
-                    {
-                        FixToFirstTrackedSkeleton();
-                        return GetSkeleton(trackedId);
-                    }
-
-                    return s;
-                }
-                else
+                switch (_tracking)
                 {
-                    return GetNearestTrackedSkeleton();
+                    case SkeletonType.Nearest:
+                        return GetNearestKnownSkeleton();
+                    case SkeletonType.NearestTracked:
+                        return GetNearestTrackedSkeleton();
+                    case SkeletonType.FixById:
+                        return GetSkeleton(_trackedSkeletonId);
+                    case SkeletonType.FixByIdOrAny:
+                        skeleton = GetSkeleton(_trackedSkeletonId);
+                        if (skeleton == null)
+                        {
+                            FixToFirstKnownSkeleton();
+                            return GetSkeleton(_trackedSkeletonId);
+                        }
+
+                        return skeleton;
+                    case SkeletonType.FixByIdOrAnyTracked:
+                        skeleton = GetSkeleton(_trackedSkeletonId);
+                        if (skeleton == null)
+                        {
+                            FixToFirstTrackedSkeleton();
+                            return GetSkeleton(_trackedSkeletonId);
+                        }
+
+                        return skeleton;
                 }
+
+                return null;
             }
         }
 
@@ -47,13 +92,32 @@ namespace Mach.Kinect
             _timestamp = skeletonTimestamp;
         }
 
+        private void FixToFirstKnownSkeleton()
+        {
+            Skeleton skeleton = GetAnyKnownkeleton();
+            if (skeleton != null)
+            {
+                _trackedSkeletonId = skeleton.TrackingId;
+            }
+        }
+
         private void FixToFirstTrackedSkeleton()
         {
             Skeleton skeleton = GetAnyTrackedSkeleton();
             if (skeleton != null)
             {
-                trackedId = skeleton.TrackingId;
+                _trackedSkeletonId = skeleton.TrackingId;
             }
+        }
+
+        public Skeleton GetAnyKnownkeleton()
+        {
+            if (Items == null)
+                return null;
+
+            return Items
+                .Where(s => s.TrackingState == SkeletonTrackingState.Tracked || s.TrackingState == SkeletonTrackingState.PositionOnly)
+                .FirstOrDefault();
         }
 
         public Skeleton GetAnyTrackedSkeleton()
@@ -61,7 +125,20 @@ namespace Mach.Kinect
             if (Items == null)
                 return null;
 
-            return Items.Where(s => s.TrackingState == SkeletonTrackingState.Tracked).FirstOrDefault();
+            return Items
+                .Where(s => s.TrackingState == SkeletonTrackingState.Tracked)
+                .FirstOrDefault();
+        }
+
+        public Skeleton GetNearestKnownSkeleton()
+        {
+            if (Items == null)
+                return null;
+
+            return Items
+                .Where(s => s.TrackingState == SkeletonTrackingState.Tracked || s.TrackingState == SkeletonTrackingState.PositionOnly)
+                .OrderBy(s => s.Position.Z)
+                .FirstOrDefault();
         }
 
         public Skeleton GetNearestTrackedSkeleton()
@@ -69,7 +146,10 @@ namespace Mach.Kinect
             if (Items == null)
                 return null;
 
-            return Items.Where(s => s.TrackingState == SkeletonTrackingState.Tracked).OrderBy(s => s.Position.Z).FirstOrDefault();
+            return Items
+                .Where(s => s.TrackingState == SkeletonTrackingState.Tracked)
+                .OrderBy(s => s.Position.Z)
+                .FirstOrDefault();
         }
 
         public Skeleton GetSkeleton(int trackingId)
@@ -77,7 +157,9 @@ namespace Mach.Kinect
             if (Items == null)
                 return null;
 
-            return Items.Where(s => s.TrackingId == trackedId).FirstOrDefault();
+            return Items
+                .Where(s => s.TrackingId == _trackedSkeletonId)
+                .FirstOrDefault();
         }
     }
 }
