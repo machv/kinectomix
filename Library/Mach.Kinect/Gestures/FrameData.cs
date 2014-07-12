d3ad69b5319fc5d8ab4b9241dd8ba69e5aabc1d1
Vladim�r Mach <vladimir@mach.im>
2014-07-12 23:07:20 +0000

    Documentation.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C41926

--- Library/Mach.Kinect/Gestures/FrameData.cs
+++ Library/Mach.Kinect/Gestures/FrameData.cs
@@ -2,16 +2,29 @@
 
 namespace Mach.Kinect.Gestures
 {
+    /// <summary>
+    /// Stores recorded joints for one frame.
+    /// </summary>
     public class FrameData
     {
-        public SkeletonPoint[] SkeletonJoints { get; set; }
+        private SkeletonPoint[] _skeletonJoints;
 
-        public int Capacity { get; private set; }
+        /// <summary>
+        /// Gets saved point coordinates for skeleton joints.
+        /// </summary>
+        /// <returns>Saved point coordinates for skeleton joints.</returns>
+        public SkeletonPoint[] SkeletonJoints
+        {
+            get { return _skeletonJoints; }
+        }
 
+        /// <summary>
+        /// Initializes new instance of <see cref="FrameData"/> class with defined skeleton joints capacity.
+        /// </summary>
+        /// <param name="capacity"></param>
         public FrameData(int capacity)
         {
-            Capacity = capacity;
-            SkeletonJoints = new SkeletonPoint[capacity];
+            _skeletonJoints = new SkeletonPoint[capacity];
         }
     }
 }
