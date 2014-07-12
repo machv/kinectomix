2e0f7c7faae9255316c07fb63765aee049d18526
Vladim�r Mach <vladimir@mach.im>
2014-07-12 22:59:48 +0000

    Documenting Gestures.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C41925

--- Library/Mach.Kinect/Gestures/Recorder.cs
+++ Library/Mach.Kinect/Gestures/Recorder.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections.Generic;
 using Microsoft.Kinect;
+using System;
 
 namespace Mach.Kinect.Gestures
 {
+    /// <summary>
+    /// Records new gesture.
+    /// </summary>
     public class Recorder : GestureProcessor
     {
         private IEnumerable<JointType> _trackedJoints;
         private TrackingDimension _dimension;
+        private bool _isStarted;
 
+        /// <summary>
+        /// Starts recording frames for a new gesture into the frame buffer until <see cref="GetRecordedGesture"/> method is used for retrieve recorded gesture.
+        /// If called again before previous gesture is retrieved via <see cref="GetRecordedGesture"/>, recorded frames will be flushed.
+        /// </summary>
+        /// <param name="trackedJoints">Collection of significant joints that will be tracked for the gesture.</param>
+        /// <param name="dimension">Tracking dimension for points of tracked joints.</param>
         public void Start(IEnumerable<JointType> trackedJoints, TrackingDimension dimension)
         {
             Clear();
@@ -15,15 +26,34 @@ namespace Mach.Kinect.Gestures
             _maximalBufferLength = int.MaxValue;
             _trackedJoints = trackedJoints;
             _dimension = dimension;
+            _isStarted = true;
         }
 
+        /// <summary>
+        /// Returns new gesture created from the recorded frames since <see cref="Start(IEnumerable{JointType}, TrackingDimension)"/> was called.
+        /// </summary>
+        /// <returns>Recorded gesture.</returns>
         public Gesture GetRecordedGesture()
         {
+            if (_isStarted == false)
+                throw new InvalidOperationException("Recording is not started.");
+
+            _isStarted = false;
+
             return Gesture.FromFrameData(_frameBuffer, _trackedJoints, _dimension);
         }
 
+        /// <summary>
+        /// Extracts <see cref="Skeleton"/> points to gesture frame buffer.
+        /// </summary>
+        /// <param name="skeleton">Skeleton retrieved from the Kinect sensor.</param>
         public override void ProcessSkeleton(Skeleton skeleton)
         {
+            if (!_isStarted)
+            {
+                return;
+            }
+
             base.ProcessSkeleton(skeleton);
         }
     }
--- Library/Mach.Kinect/Gestures/TrackingDimension.cs
+++ Library/Mach.Kinect/Gestures/TrackingDimension.cs
@@ -1,10 +1,17 @@
-﻿using System;
-
-namespace Mach.Kinect.Gestures
+﻿namespace Mach.Kinect.Gestures
 {
+    /// <summary>
+    /// Specifies what dimension for points is used when processing gestures.
+    /// </summary>
     public enum TrackingDimension
     {
+        /// <summary>
+        /// Use only x and y axes.
+        /// </summary>
         Two = 2,
+        /// <summary>
+        /// Use all x, y and z axes. Will use also depth information.
+        /// </summary>
         Three = 3,
     }
 }
