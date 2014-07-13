60afd2bea21e575ec64c25126e6141e9c334560c
Vladim�r Mach <vladimir@mach.im>
2014-07-13 10:32:04 +0000

    Documentation.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C41931

--- Library/Mach.Kinect/Gestures/RecognizedGestureComparer.cs
+++ Library/Mach.Kinect/Gestures/RecognizedGestureComparer.cs
@@ -2,16 +2,30 @@
 
 namespace Mach.Kinect.Gestures
 {
+    /// <summary>
+    /// Compares two <see cref="RecognizedGesture"/>s if same gesture is recognized.
+    /// </summary>
     public class RecognizedGestureComparer : IEqualityComparer<RecognizedGesture>
     {
-        public bool Equals(RecognizedGesture x, RecognizedGesture y)
+        /// <summary>
+        /// Determines whether the specified <see cref="RecognizedGesture"/>s contains same <see cref="Gesture"/>.
+        /// </summary>
+        /// <param name="gesture1">First gesture to compare.</param>
+        /// <param name="gesture2">Second gesture to compare.</param>
+        /// <returns>True if both <see cref="RecognizedGesture"/> recognized same gesture.</returns>
+        public bool Equals(RecognizedGesture gesture1, RecognizedGesture gesture2)
         {
-            return x.Gesture.Equals(y.Gesture);
+            return gesture1.Gesture.Equals(gesture2.Gesture);
         }
 
-        public int GetHashCode(RecognizedGesture obj)
+        /// <summary>
+        /// Returns a hash code for the specified <see cref="RecognizedGesture"/>.
+        /// </summary>
+        /// <param name="gesture">The <see cref="RecognizedGesture"/> for which a hash code is to be returned.</param>
+        /// <returns>A hash code for the specified <see cref="RecognizedGesture"/>.</returns>
+        public int GetHashCode(RecognizedGesture gesture)
         {
-            return obj.Gesture.GetHashCode();
+            return gesture.Gesture.GetHashCode();
         }
     }
 }
