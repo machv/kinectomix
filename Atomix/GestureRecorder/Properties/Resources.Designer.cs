451f596c65c23e3ac80170cac21fb408b7cc4973
Vladim�r Mach <vladimir@mach.im>
2014-07-17 14:26:55 +0000

    Renaming.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/Atomix;C41970

--- Atomix/GestureRecorder/Properties/Resources.Designer.cs
+++ Atomix/GestureRecorder/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Kinectomix.GestureRecorder.Properties {
+namespace Mach.Kinectomix.GestureRecorder.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Kinectomix.GestureRecorder.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Kinectomix.GestureRecorder.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Mach.Kinectomix.GestureRecorder.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
